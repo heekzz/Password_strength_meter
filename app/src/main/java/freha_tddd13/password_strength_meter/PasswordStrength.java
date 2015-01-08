7f417e32978ca81452fb26fea037f63f68c94e7d
heekzz <f.hakansson@hotmail.se>
2015-01-08 12:20:40 +0100

    WOOP klar men allting kan bara visualiseras på ett sätt. Finns inga alternativ till hur styrkan ska visas. Men den fungerar med en hyfsad algoritm för lösenordet

--- app/src/main/java/freha_tddd13/password_strength_meter/PasswordStrength.java
+++ app/src/main/java/freha_tddd13/password_strength_meter/PasswordStrength.java
@@ -1,20 +1,32 @@
 package freha_tddd13.password_strength_meter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PasswordStrength extends LinearLayout {
 
     private EditText textField;
-    private Line lines;
+    private ProgressBar progressBar;
+    private TextView strengthText; // Prints "Password strength"
+    private TextView strengthTextHint; // Prints how strong the password is
+    private ColorFilter defaultProgBarColor;
     Context context;
+
+    // Minimum length of password
+    private int minimumPasswordLength = 6;
 
     public PasswordStrength(Context context) {
         super(context);
@@ -31,9 +43,27 @@ public class PasswordStrength extends LinearLayout {
     private void init() {
         setOrientation(VERTICAL);
         textField = new EditText(context);
-        lines = new Line(context);
+
+        // Create a horizontal thin progressbar
+        progressBar = new ProgressBar(context, null, android.R.attr.progressBarStyleHorizontal);
         LayoutParams params = new LayoutParams(
                 LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+
+        progressBar.setMax(4);
+        textField.setHint("Minimum of " + minimumPasswordLength + " chars in length");
+
+        LinearLayout textLayout = new LinearLayout(context);
+        textLayout.setOrientation(HORIZONTAL);
+        strengthTextHint = new TextView(context);
+        strengthTextHint.setText("TOO SHORT");
+        strengthText = new TextView(context);
+        strengthText.setText("Password Strength: ");
+
+        textLayout.addView(strengthText);
+        textLayout.addView(strengthTextHint);
+
+        // Saves the default color of the progressbar so w
+        defaultProgBarColor = progressBar.getProgressDrawable().getColorFilter();
 
         textField.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_PASSWORD);
         textField.addTextChangedListener(new TextWatcher() {
@@ -44,8 +74,8 @@ public class PasswordStrength extends LinearLayout {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                int x = getDifficulty(s.toString());
-                lines.setSecurityLevel(x);
+                int x = getStrength(s.toString());
+                setStrength(x);
             }
 
             @Override
@@ -53,51 +83,94 @@ public class PasswordStrength extends LinearLayout {
 
             }
         });
+        progressBar.setLayoutParams(params);
         textField.setLayoutParams(params);
-        lines.setLayoutParams(params);
-        addView(textField);
-        addView(lines);
-    }
 
-    // Setters....
-    public void setLineHeight(float lineHeight) {
-        lines.setLineHeight(lineHeight);
-    }
-    public void setMinimumLength(int length) {
-        lines.setMinimumTextLength(length);
-    }
-    public void setMessageLevelOne(String message) {
-        lines.setMessageLevelOne(message);
-    }
-    public void setMessageLevelTwo(String message) {
-        lines.setMessageLevelTwo(message);
-    }
-    public void setMessageLevelThree(String message) {
-        lines.setMessageLevelThree(message);
+        addView(textField);
+        addView(textLayout);
+        addView(progressBar);
     }
 
     /**
-     * Get the difficulty level for the password typed in the text field
-     * Override this to change the algorithm for your Password Strength Meter.
-     * 0 - Too short password
-     * 1 - Too weak password, not accepted
-     * 2 - Accepted password. Recommend a stronger though
-     * 3 - A strong password!
-     * @param password String which is your typed password
-     * @return Int between 0-3 depending on difficulty on input
+     * Sets minimum length of the password
+     * @param minimumLength minimum length of password
      */
-    protected int getDifficulty(String password) {
-        int min = lines.getMinimumTextLength();
-        if(password.length() < min){
-            return 0;
-        } else if (password.length() == min) {
-            return 1;
-        } else if (password.length() > min && password.length() <= min + 3) {
-            return 2;
-        } else if(password.length() > min + 3){
-            return 3;
-        } else
-            return  3;
+    public void setMinimumLength(int minimumLength) {
+        this.minimumPasswordLength = minimumLength;
+    }
+
+
+    /**
+     * Returns a integer value representing the strength of the password.
+     * The value can go from 0 to 4 or bigger but the component only changes on the values 0-4.
+     * Values higher than 4 gives the same result as 4.
+     * @param password Password you want to check strength of
+     * @return integer values from 0 and bigger, 0 = too short password,
+     * 1 = weak password, 2 = fair password, 3 = good password, 4 = strong password.
+     */
+    protected int getStrength(String password) {
+        int strength = 0;
+        Pattern pattern1 = Pattern.compile("([A-Z])"); // Contains a uppercase letter
+        Pattern pattern2 = Pattern.compile("([!#€%&/()=?)])"); // Contains a special character
+        Matcher matcher1 = pattern1.matcher(password);
+        Matcher matcher2 = pattern2.matcher(password);
+
+        /* Must always be at least longer than the minimum length
+           For every criteria the password fulfills we increase the strength */
+        if (password.length() >= minimumPasswordLength) {
+            strength++;
+            if (password.length() >= 12)
+                strength++;
+            if (matcher1.find())
+                strength++;
+            if (matcher2.find())
+                strength++;
+        }
+        return strength;
+
+    }
+
+    /**
+     * Method that handles all the graphic parts. Changing the color of the progressbar
+     * depending on what value is received. Values between 0-4 are handled.
+     * Values can be bigger than 4 but will generate the same result as the value 4.
+     * 0 prints too short pw depending on the minimum length set for the field
+     * 1 prints status of a weak password
+     * 2 prints status of a fair password
+     * 3 prints status of a good password
+     * 4 prints status of a strong password
+     * @param strength 0-4 prints messages of strength
+     */
+    protected void setStrength(int strength) {
+        progressBar.setProgress(strength);
+        switch (strength) {
+            case 0:
+                strengthTextHint.setText("TOO SHORT");
+                strengthTextHint.setTextColor(Color.GRAY);
+                progressBar.getProgressDrawable().setColorFilter(defaultProgBarColor);
+                break;
+            case 1:
+                strengthTextHint.setText("WEAK");
+                strengthTextHint.setTextColor(Color.RED);
+                progressBar.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+                break;
+            case 2:
+                strengthTextHint.setText("FAIR");
+                strengthTextHint.setTextColor(Color.parseColor("#CCCC00"));
+                progressBar.getProgressDrawable().setColorFilter(Color.parseColor("#CCCC00"), PorterDuff.Mode.SRC_IN);
+                break;
+            case 3:
+                strengthTextHint.setText("GOOD");
+                strengthTextHint.setTextColor(Color.parseColor("#3399FF"));
+                progressBar.getProgressDrawable().setColorFilter(Color.parseColor("#3399FF"), PorterDuff.Mode.SRC_IN);
+                break;
+            default:
+                strengthTextHint.setText("STRONG");
+                strengthTextHint.setTextColor(Color.parseColor("#66CC00"));
+                progressBar.getProgressDrawable().setColorFilter(Color.parseColor("#66CC00"), PorterDuff.Mode.SRC_IN);
+                break;
+        }
+
     }
 
 
